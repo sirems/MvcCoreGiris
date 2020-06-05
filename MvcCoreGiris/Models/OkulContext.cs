58bd8c9fbf73f448759c37b96c23255bba7a1dad
sirems <siremsoydan@icloud.com>
2020-06-05 22:12:36 +0300

    Add project files.

---
+++ MvcCoreGiris/Models/OkulContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcCoreGiris.Models
+{
+    public class OkulContext:DbContext
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=.;database=OkulContextDb;uid=sa;pwd=123irem456");
+        //}
+        public OkulContext(DbContextOptions<OkulContext>options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kisi>().HasData(
+                new Kisi{Id = 1,KisiAd = "irem"}, 
+                new Kisi{Id = 2,KisiAd = "meri"});
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Kisi> Kisiler { get; set; }
+
+    }
+}
